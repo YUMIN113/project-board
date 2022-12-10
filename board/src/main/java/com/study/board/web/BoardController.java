0f579a2f3ef3c038428ede4e7b2c738ee9766ef5
yumin <cfy113@naver.com>
2022-12-10 23:33:39 +0900

    Add: CRUD_Read_list/detail_Controller 구현

--- board/src/main/java/com/study/board/web/BoardController.java
+++ board/src/main/java/com/study/board/web/BoardController.java
@@ -1,9 +1,14 @@
 package com.study.board.web;
 
+import com.study.board.dto.BoardResponseDto;
 import com.study.board.dto.BoardSaveRequestDto;
 import com.study.board.service.BoardService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RequestMapping("/")
 @Controller
@@ -17,7 +22,25 @@ public class BoardController {
 
     @PostMapping("form")
     public String save(BoardSaveRequestDto boardSaveRequestDto) {
-        boardService.save(boardSaveRequestDto);
-        return "redirect:/";
+        boardService.enrollBoard(boardSaveRequestDto);
+        return "redirect:/list";
+    }
+
+    @GetMapping("list")
+    public void list(Model model) {
+        model.addAttribute("boardResponseDtos", boardService.getBoardResponseDtoList());
+    }
+
+    @GetMapping("detail")
+    public Map detail(Long no) throws Exception {
+
+       BoardResponseDto boardResponseDto = boardService.getBoardResponseDtoNo(no);
+       if (boardResponseDto == null) {
+           throw new Exception("해당 번호의 게시글이 없습니다!");
+       }
+
+       Map map = new HashMap();
+       map.put("boardResponseDto", boardResponseDto);
+       return map;
     }
 }
