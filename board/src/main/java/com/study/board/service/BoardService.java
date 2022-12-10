358481a3d2e3b0945b7eb809301ab1080c5811c4
yumin <cfy113@naver.com>
2022-12-10 23:35:13 +0900

    Add: CRUD_Read_list/detail_Service 추가

--- board/src/main/java/com/study/board/service/BoardService.java
+++ board/src/main/java/com/study/board/service/BoardService.java
@@ -1,9 +1,14 @@
 package com.study.board.service;
 
-
-import com.study.board.domain.BoardRepository;
+import com.study.board.domain.Board;
+import com.study.board.dto.BoardResponseDto;
+import com.study.board.repository.BoardRepository;
 import com.study.board.dto.BoardSaveRequestDto;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 public class BoardService {
@@ -14,7 +19,27 @@ public class BoardService {
         this.boardRepository = boardRepository;
     }
 
-    public Long save(BoardSaveRequestDto boardSaveRequestDto) {
-        return boardRepository.save(boardSaveRequestDto.toEntity()).getNo();
+    public void enrollBoard(BoardSaveRequestDto boardSaveRequestDto) {
+        boardRepository.save(boardSaveRequestDto.toEntity());
+    }
+
+    // BoardRepository 는 Board 형이기 때문에 BoardResponseDto 로 형변환 해야 함.
+    public List<BoardResponseDto> getBoardResponseDtoList() {
+        return boardRepository.findAll().stream().map(it -> {
+            return new BoardResponseDto(it);
+        }).collect(Collectors.toList());
+    }
+
+    // BoardRepository 는 Board 형이기 때문에 BoardResponseDto 로 형변환 해야 함.
+    public BoardResponseDto getBoardResponseDtoNo(Long no) {
+        Optional<Board> boardWrapper = boardRepository.findById(no);
+        Board board = boardWrapper.get();
+
+        return BoardResponseDto.builder()
+                .no(board.getNo())
+                .title(board.getTitle())
+                .writer(board.getWriter())
+                .content(board.getContent())
+                .build();
     }
 }
